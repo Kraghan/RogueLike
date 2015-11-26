1fb1392c017caf96860bc733b3526044f19f4ada
Renaud Belloncle <renaud.belloncle@orange.fr>
2015-11-26 17:09:22 +0100

    Initial commit

---
+++ desktop/src/com/mygdx/roguelike/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.roguelike.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.roguelike.RogueLike;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RogueLike(), config);
+	}
+}
